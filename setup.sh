c3bbed34a417f604f25678cba180b3adde420627
fajemila <fajemild@gmail.com>
2023-08-04 00:23:16 +0100

    Setup Heroku

--- setup.sh
+++ setup.sh
@@ -6,9 +6,9 @@ port = $PORT\n\
 enableCORS = false\n\
 \n\
 [theme]\n\
-primaryColor = \"#8DB600\"\n\
-backgroundColor = \"#6E7F80\"\n\
-secondaryBackgroundColor = \"#A4C639\"\n\
-textColor = \"#FFFFFF\"\n\
-font = \"sans-serif\"\n\
+primaryColor = \"#0E8388\"\n\
+backgroundColor = \"#2C3333\"\n\
+secondaryBackgroundColor = \"#2E4F4F\"\n\
+textColor = \"#CBE4DE\"\n\
+font = \"monospace\"\n\
 " > ~/.streamlit/config.toml
